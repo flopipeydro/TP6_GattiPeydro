6be123e2629fff56694136dab22c5787acfbf416
flopipeydro <107050166+flopipeydro@users.noreply.github.com>
2024-11-25 17:33:39 -0300

    Create buscar_paciente.py

---
+++ Actividad3/buscar_paciente.py
@@ -0,0 +1,18 @@
+import requests
+def search_patient_manually(document_id, system_url):
+    search_url = f"{BASE_URL}/Patient"
+    response = requests.get(search_url, headers={"Content-Type": "application/json"})
+    
+    if response.status_code == 200:
+        all_patients = response.json().get("entry", [])
+        for patient in all_patients:
+            resource = patient.get("resource", {})
+            identifiers = resource.get("identifier", [])
+            for identifier in identifiers:
+                if identifier["system"] == system_url and identifier["value"] == document_id:
+                    print("Paciente encontrado:")
+                    print(json.dumps(resource, indent=2))
+                    return
+        print(f"No se encontró ningún paciente con el documento {document_id}.")
+    else:
+        print("Error al buscar pacientes:", response.status_code, response.text)
