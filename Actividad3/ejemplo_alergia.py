d276d45347f4fe519b034b7d6f67857d846a98e3
flopipeydro <107050166+flopipeydro@users.noreply.github.com>
2024-11-25 20:06:25 -0300

    Create ejemplo_alergia.py

--- Actividad3/ejemplo_alergia.py
+++ Actividad3/ejemplo_alergia.py
@@ -1,4 +1,4 @@
-patient_id = "2595883"  # ID del paciente previamente creado
+patient_id = "2595978"  # ID del paciente previamente creado
 substance_code = "1840032"  # Código para Penicilina
 substance_display = "Penicilina"  # Nombre de la sustancia
 criticality = "high"  # Nivel de gravedad
@@ -7,7 +7,6 @@ manifestation_code = "77386006"  # Código de manifestación Presion alta
 manifestation_display = "Shock disorder or hypotension"  # Descripción de la manifestación
 severity = "severe"  # Severidad de la reacción
 note = "El paciente tiene una reacción severa en la presion tras el consumo de penicilina."  # Nota descriptiva
-
 
 allergy_dict = create_allergy_intolerance_resource(
     patient_id, substance_code, substance_display, criticality, category,
